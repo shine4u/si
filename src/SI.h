bdccfabb2da82c1cef83c03778dbab763317b845
shine4u <shine19890705@gmail.com>
2014-04-04 11:37:02 +0800

    Create SI.h

---
+++ src/SI.h
@@ -0,0 +1,12 @@
+#ifndef _SI_H_
+#define _SI_H_
+
+#include "BasicDefs.h"
+#include "Logger.h"
+#include "TypeCvtor.h"
+#include "StrHelper.h"
+#include "Configer.h"
+#include "TimeDate.h"
+#include "LineParser.h"
+
+#endif // _SI_H_
